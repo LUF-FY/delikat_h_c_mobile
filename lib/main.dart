6e46be6aca9378aa75dd19b9f48f404a60347e3c
luf-fy <margiev.erik@list.ru>
2023-03-17 00:38:51 +0300

    add product card widget, assets. divided the main page into common widgets

--- lib/main.dart
+++ lib/main.dart
@@ -57,46 +57,111 @@ class _MainPageState extends State<MainPage> {
           style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
         ),
         actions: [
-          IconButton(
-            onPressed: (() {}),
-            icon: const Icon(
-              Icons.search,
-              color: Colors.black,
-            ),
-          ),
-          Stack(
-            children: [
-              IconButton(
-                onPressed: (() {}),
-                icon: const Icon(
-                  Icons.shopping_cart,
-                  color: Colors.black,
-                ),
-              ),
-              Positioned(
-                top: 0,
-                right: 3,
-                child: Container(
-                  height: 20,
-                  width: 20,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.red,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "0",
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AppBarActoins(),
           ),
         ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            childAspectRatio: 0.6,
+          ),
+          itemBuilder: (context, index) {
+            //print("build");
+            //return ItemWidget(product: _products[index]);
+          },
+          //itemCount: _products.length,
+        ),
+      ),
+    );
+  }
+}
+
+class AppBarActoins extends StatelessWidget {
+  const AppBarActoins({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButtonSearch(),
+        Stack(
+          children: [
+            IconButtonCart(),
+            Positioned(
+              top: 0,
+              right: 3,
+              child: CounterProductsInCart(),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class IconButtonSearch extends StatelessWidget {
+  const IconButtonSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+// Открыть текстовое поле
+
+      onPressed: (() {}),
+      icon: const Icon(
+        Icons.search,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class IconButtonCart extends StatelessWidget {
+  const IconButtonCart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+// По нажатию переходить на страницу корзины
+
+      onPressed: (() {}),
+      icon: const Icon(
+        Icons.shopping_cart,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class CounterProductsInCart extends StatelessWidget {
+  const CounterProductsInCart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      width: 20,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.red,
+      ),
+      child: const Center(
+        child: Text(
+// Нужно менять значение
+
+          "0",
+          style: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
       ),
     );
   }
