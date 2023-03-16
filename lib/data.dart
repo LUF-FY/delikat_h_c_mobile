8e8c25f85efd2aa1e1a747a1a0b480827ecab3ef
luf-fy <margiev.erik@list.ru>
2023-03-17 00:39:58 +0300

    -=-

---
+++ lib/data.dart
@@ -0,0 +1,19 @@
+import 'product_class.dart';
+
+final List<Product> allData = [
+  Product(
+    id: 1,
+    name: 'Эрик будущий депутат',
+    price: 0,
+    img: 'assets/image1.png',
+    description: 'Любит тяночек',
+  ),
+  Product(
+    id: 2,
+    name: 'OLINA',
+    price: 6,
+    img: 'assets/image2.png',
+    description:
+        ',Хочет пони минижирафа самоеда корги много денег дом квартиру машину еще машину и еще дом и еще квартиру и поездить по миру но ничего не делатьа почему а потому что ВОВА КУПИ',
+  ),
+];
