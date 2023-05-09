26efe71a94727fbe9ec715a75f5a861dbbf605d9
luf-fy <margiev.erik@list.ru>
2023-05-09 19:28:14 +0300

    Change Utils

--- lib/ui/widgets/utils.dart
+++ lib/ui/widgets/utils.dart
@@ -4,11 +4,11 @@ class Utils {
   static GlobalKey<NavigatorState> mainListNav = GlobalKey();
   static GlobalKey<NavigatorState> mainAppNav = GlobalKey();
 
-  static const Color mainColor = Color(0xFF4CAF50);
-  static const Color mainDark = Color(0xFF2A612B);
+  static const Color mainGreen = Color.fromRGBO(76, 175, 80, 1);
+  static const Color mainGreenDark = Color(0xFF2A612B);
   static const Color buttonDisableBG = Color(0xFFEEEEEE);
   static const Color buttonDisableT = Colors.grey;
   static const Color buttonEnableT = Colors.white;
-  static const Color mainTextColor = Colors.white;
-  static const Color mainDarkTextColor = Colors.black;
+  static const Color mainWhite = Colors.white;
+  static const Color mainBlack = Colors.black;
 }
