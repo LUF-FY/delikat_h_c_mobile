4c3bb94b7e7da744aa85b671e7503d27616a8387
luf-fy <margiev.erik@list.ru>
2023-05-11 22:38:54 +0300

    Change color

--- lib/ui/widgets/utils.dart
+++ lib/ui/widgets/utils.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Utils {
-  static const Color mainGreen = Color.fromRGBO(76, 175, 80, 1);
-  static const Color mainGreenDark = Color(0xFF2A612B);
+  static const Color mainGreen = Color.fromRGBO(73, 148, 206, 1);
+  static const Color mainGreenDark = Color.fromARGB(255, 44, 89, 141);
   static const Color buttonDisableBG = Color(0xFFEEEEEE);
   static const Color buttonDisableT = Colors.grey;
   static const Color buttonEnableT = Colors.white;
