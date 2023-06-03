b8af73a63346108e658ba22c6d2b0884c5f3a375
luf-fy <margiev.erik@list.ru>
2023-06-03 19:45:48 +0300

    changed logo and app name

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\user\\flutterProjects\\delikat_h_c_mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\user\\flutterProjects\\delikat_h_c_mobile"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\user\\flutterProjects\\delikat_h_c_mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\user\\flutterProjects\\delikat_h_c_mobile"
+  "FLUTTER_TARGET=C:\\Users\\user\\flutterProjects\\delikat_h_c_mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\user\\flutterProjects\\delikat_h_c_mobile\\.dart_tool\\package_config.json"
+)
